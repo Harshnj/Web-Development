34fbf8cf43a59ab919710f5c42573841c0343418
Harshnj <95063086+Harshnj@users.noreply.github.com>
2024-01-22 23:48:47 +0530

    Create const.js

---
+++ Front_End/Javascript/DataTypes/const.js
@@ -0,0 +1,5 @@
+//const in javascirpt is a keyword whcih is used to non changing variables data
+//If the variable is declared const then we cannot change the value of it
+const a =10;
+a = 11;//casuse error
+console.log("a");
